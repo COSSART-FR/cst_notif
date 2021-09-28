caa8b97c574372ec8000df6a6ab0e4d027110105
Wesley <31973315+SUP2Ak@users.noreply.github.com>
2021-09-28 23:07:32 +0200

    #correct .js event SendNUIMessage

--- client.lua
+++ client.lua
@@ -1,5 +1,14 @@
 RegisterCommand("notif", function()
-	SendNUIMessage({
-		createNotification()
-	})
+	cstNotif()
 end, false)
+
+function cstNotif()
+	--Citizen.CreateThread(function()
+	--	while true do
+	--		Citizen.Wait(5.0)
+			SendNUIMessage({
+				action = true
+			})
+	--	end
+	--end)
+end
